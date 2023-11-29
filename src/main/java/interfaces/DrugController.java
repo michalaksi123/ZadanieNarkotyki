627ea7dbc88d65af5bab93c88f95dcc764e0af90
Gabrysia Gołąb <gabriela.golab.1998@gmail.com>
2023-11-29 19:55:19 +0100

    commit init

--- src/main/java/interfaces/DrugController.java
+++ src/main/java/interfaces/DrugController.java
@@ -1,7 +1,14 @@
 package interfaces;
 
+import enums.Jakosc;
 import exceptions.BadDrugQualityException;
+import model.Narkotyk;
 
 public interface DrugController {
-    void checkDrug();
+
+    static void checkDrug(Narkotyk narkotyk){
+        if(narkotyk.getJakosc() == Jakosc.ZLA) {
+            throw new BadDrugQualityException("Zła jakość narkotyku");
+        }
+    }
 }
--- src/main/java/model/Narkotyk.java
+++ src/main/java/model/Narkotyk.java
@@ -11,119 +11,72 @@ import java.util.List;
 public abstract class Narkotyk {
 
     private String nazwa;
-    private double CENA_ZA_GRAM = 30;
     private double cenaZagram;
     private List<String> skladniki;
     private String rodzaj;
     private Jakosc jakosc;
-    private DrugController drugController;
     private List<Dziecko> dzieci = new ArrayList<>();
 
     public Narkotyk(String nazwa, double cenaZagram, List<String> skladniki, String rodzaj, Jakosc jakosc) {
-
         if (skladniki.size() < 3) {
             throw new NotEnoughIngredientsException("Narkotyk musi mieć conajmniej 3 składniki!");
         }
-
         this.nazwa = nazwa;
         this.cenaZagram = cenaZagram;
         this.skladniki = skladniki;
         this.rodzaj = rodzaj;
         this.jakosc = jakosc;
-        this.dzieci = new ArrayList<>();
     }
 
-    public void checkDrug() {
-        drugController.checkDrug();
-        if (jakosc.equals(Jakosc.ZLA)) {
-            throw new BadDrugQualityException("Narkotyk ma zła jakość");
-        } else {
-            System.out.println("Dobry temacik ");
-        }
-    }
 
-    public void wzrostCeny() {
-        if (nazwa.equalsIgnoreCase("LSD") && skladniki.size() > 5) {
-            CENA_ZA_GRAM += 60;
-        }
-        if (nazwa.equalsIgnoreCase("LSD") && skladniki.size() < 5) {
-            CENA_ZA_GRAM += 30;
-        }
-        if (nazwa.equalsIgnoreCase("MDMA") && skladniki.size() > 4){
-            CENA_ZA_GRAM += 30;
-        }
-        if (nazwa.equalsIgnoreCase("MDMA") && skladniki.size() < 4){
-            CENA_ZA_GRAM += 20;
-        }
-        if (nazwa.equalsIgnoreCase("MDMA") && skladniki.size() > 4){
-            CENA_ZA_GRAM += 30;
-        }
-        if (nazwa.equalsIgnoreCase("MDMA") && skladniki.size() < 4){
-            CENA_ZA_GRAM += 20;
 
-        }
-        if (nazwa.equalsIgnoreCase("Mefedron") && skladniki.size() > 5){
-            CENA_ZA_GRAM += 60;
-        }
-        if (nazwa.equalsIgnoreCase("Mefedron") && skladniki.size() < 5){
-            CENA_ZA_GRAM += 40;
-        }
 
-    }
-
-    public String getNazwa() {
+public String getNazwa() {
         return nazwa;
-    }
+        }
 
-    public void setNazwa(String nazwa) {
+public void setNazwa(String nazwa) {
         this.nazwa = nazwa;
-    }
+        }
 
-    public double getCenaZagram() {
+public double getCenaZagram() {
         return cenaZagram;
-    }
+        }
 
-    public void setCenaZagram(double cenaZagram) {
+public void setCenaZagram(double cenaZagram) {
         this.cenaZagram = cenaZagram;
-    }
+        }
 
-    public List<String> getSkladniki() {
+public List<String> getSkladniki() {
         return skladniki;
-    }
+        }
 
-    public void setSkladniki(List<String> skladniki) {
+public void setSkladniki(List<String> skladniki) {
         this.skladniki = skladniki;
-    }
+        }
 
-    public String getRodzaj() {
+public String getRodzaj() {
         return rodzaj;
-    }
+        }
 
-    public void setRodzaj(String rodzaj) {
+public void setRodzaj(String rodzaj) {
         this.rodzaj = rodzaj;
-    }
+        }
 
-    public Jakosc getJakosc() {
+public Jakosc getJakosc() {
         return jakosc;
-    }
+        }
 
-    public void setJakosc(Jakosc jakosc) {
+public void setJakosc(Jakosc jakosc) {
         this.jakosc = jakosc;
-    }
+        }
 
-    public List<Dziecko> getDzieci() {
+public List<Dziecko> getDzieci() {
         return dzieci;
-    }
+        }
 
-    public void setDzieci(List<Dziecko> dzieci) {
-        this.dzieci = dzieci;
-    }
 
-    public double getCENA_ZA_GRAM() {
-        return CENA_ZA_GRAM;
-    }
-
-    @Override
+@Override
     public String toString() {
         return "Narkotyk{" +
                 "nazwa='" + nazwa + '\'' +
